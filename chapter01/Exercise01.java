e3890d15ffc2d9d38f5f3e0d5219d74233f28c31
tutkuince <33215575+tutkuince@users.noreply.github.com>
2017-12-22 16:35:06 +0300

    Add files via upload

---
+++ chapter01/Exercise01.java
@@ -0,0 +1,8 @@
+package chapter01;
+
+public class Exercise01 {
+
+	public static void main(String[] args) {
+        System.out.println("Welcome to Java, Welcome to Computer Science. Programming is fun.");
+	}
+}
